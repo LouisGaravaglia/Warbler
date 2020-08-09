daa3cf01c58562e35041ec15c62fa90bcab22bf9
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-08-09 15:48:44 -0700

    fixed the logout view function

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ os.environ.get('DATABASE_URL', 'postgres:///warbler'))
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
@@ -109,9 +109,8 @@ def login():
 
 @app.route('/logout')
 def logout():
-    """Handle logout of user."""
-
-    # IMPLEMENT THIS
+    do_logout()
+    return redirect('/login')
 
 
 ##############################################################################
