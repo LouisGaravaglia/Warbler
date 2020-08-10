1fe3eb34ce2791c994a445ce23e27ec299503163
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-08-10 11:44:07 -0700

    first test for message model to make sure text appears is working

---
+++ test_message_model.py
@@ -0,0 +1,74 @@
+"""Message model tests."""
+
+# Using the following to run my tests: FLASK_ENV=production python -m unittest test_message_model.py
+
+import os
+from unittest import TestCase
+from sqlalchemy.exc import IntegrityError
+from models import db, User, Message, Follows
+
+os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
+
+from app import app
+
+db.create_all()
+
+class MessageModelTestCase(TestCase):
+    """Test views for messages."""
+
+
+    def setUp(self):
+        """Create test client, add sample data."""
+
+        User.query.delete()
+        Message.query.delete()
+        Follows.query.delete()
+        
+        u1 = User.signup("test1", "email1@email.com", "password", None)
+        uid1 = 1111
+        u1.id = uid1
+
+        u2 = User.signup("test2", "email2@email.com", "password", None)
+        uid2 = 2222
+        u2.id = uid2
+
+        db.session.commit()
+
+        u1 = User.query.get(uid1)
+        u2 = User.query.get(uid2)
+
+        self.u1 = u1
+        self.uid1 = uid1
+
+        self.u2 = u2
+        self.uid2 = uid2
+
+        self.client = app.test_client()
+        
+
+    def tearDown(self):
+        res = super().tearDown()
+        db.session.rollback()
+        return res
+
+        
+    def test_message_model(self):
+        """Does basic model work?"""
+
+        msg = Message(text="My first post")
+        self.u1.messages.append(msg)
+        db.session.commit()
+
+        self.assertEqual("My first post", self.u1.messages[0].text)
+        
+
+    # Does the repr method work as expected?
+    # Does is_following successfully detect when user1 is following user2?
+    # Does is_following successfully detect when user1 is not following user2?
+    # Does is_followed_by successfully detect when user1 is followed by user2?
+    # Does is_followed_by successfully detect when user1 is not followed by user2?
+    # Does User.create successfully create a new user given valid credentials?
+    # Does User.create fail to create a new user if any of the validations (e.g. uniqueness, non-nullable fields) fail?
+    # Does User.authenticate successfully return a user when given a valid username and password?
+    # Does User.authenticate fail to return a user when the username is invalid?
+    # Does User.authenticate fail to return a user when the password is invalid?
